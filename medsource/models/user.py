2c554395dd35c0610723f27f14f071cd8705fdae
Cesar Diaz <cediazme@unal.edu.co>
2021-10-30 15:17:07 -0500

    Modelos Eps,Hospital,Paciente,Enfermero y Doctor

---
+++ medsource/models/user.py
@@ -0,0 +1,70 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+# ---------------------------------- EPS ---------------------------------------
+
+class Eps(models.Model):
+    name=models.CharField('Nombre de la Eps',max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = 'Eps'
+        verbose_name_plural='Eps'
+
+# ---------------------------------- HOSPITAL ---------------------------------------
+
+class Hospital(models.Model):
+    name = models.CharField('Nombre del Hospital',max_length=100, null=False, blank=False)
+    address = models.CharField('Dirección',max_length=100,null=False,blank=False)
+    city = models.CharField('Ciudad',max_length=100,null=False,blank=False)
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = 'Hospital'
+        verbose_name_plural='Hpspitales'
+
+# ---------------------------------- PACIENTES ---------------------------------------
+
+class Patient ( models.Model):
+    identification = models.IntegerField('Cedula', null=False , blank=False )
+    name = models.CharField('Nombre',max_length=200, null=False , blank=False )
+    date_of_birth = models.DateTimeField('Fecha de Nacimiento', null=False, blank=False)
+    phone = PhoneNumberField('Teléfono', null=True, blank=True)
+    marital_status = models.CharField('Estado Civil',max_length=200 , null=True , blank=True )
+    blood_type = models.CharField('Grupo Sanguineo',max_length=200 , null=False , blank=False )
+    eps = models.ForeignKey('Eps', on_delete=models.PROTECT, related_name='Eps', null=False , blank=False )
+    def __str__(self):
+        return str(self.identification )   
+    class Meta:
+        verbose_name = 'Paciente'
+        verbose_name_plural = 'Pacientes'
+
+# ---------------------------------- ENFERMEROS ---------------------------------------
+
+class Nurse ( models.Model):
+    identification = models.IntegerField('Cedula', null=False , blank=False )
+    name = models.CharField('Nombre',max_length=200, null=False , blank=False )
+    education = models.CharField('Formación',max_length=200 , null=False , blank=False )
+    area = models.CharField('Area',max_length=200 , null=False , blank=False )
+    hospital = models.ForeignKey('Hospital', on_delete=models.PROTECT, related_name='Hospital', null=False , blank=False )
+    def __str__(self):
+        return str(self.identification )   
+    class Meta:
+        verbose_name = 'Enfermero'
+        verbose_name_plural = 'Enfermeros'
+        
+# ---------------------------------- Doctores ---------------------------------------
+
+class Doctor ( models.Model):
+    identification = models.IntegerField('Cedula', null=False , blank=False )
+    name = models.CharField('Nombre',max_length=200, null=False , blank=False )
+    education = models.CharField('Formación',max_length=200 , null=False , blank=False )
+    specialization = models.CharField('Especialización',max_length=200 , null=True , blank=True )
+    hospital = models.ForeignKey('Hospital', on_delete=models.PROTECT, related_name='Hospital', null=False , blank=False )
+    def __str__(self):
+        return str(self.identification )   
+    class Meta:
+        verbose_name = 'Doctor'
+        verbose_name_plural = 'Doctores'
