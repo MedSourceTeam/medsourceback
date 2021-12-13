41b42b301051fbcb2b8c4f254165c65a53dbad02
Juan D. Castañeda <jcastanedao@unal.edu.co>
2021-12-13 16:41:15 -0500

    Busqueda de procedimientos

--- medsource/views/procedureListView.py
+++ medsource/views/procedureListView.py
@@ -6,3 +6,15 @@ from medsource.serializers import ProcedureSerializer
 class ProcedureListView(generics.ListAPIView):
     queryset = Procedure.objects.all()
     serializer_class = ProcedureSerializer
+
+    def get_queryset(self):
+        name = self.request.query_params.get('name', None)
+        uvr = self.request.query_params.get('uvr', None)
+        queryset = Procedure.objects.all()
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        if uvr:
+            queryset = queryset.filter(uvr__gte=uvr)
+
+        return queryset
