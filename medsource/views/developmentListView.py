151bd7c80fed7443d6181c71a946d91f7fad602d
Juan D. Castañeda <jcastanedao@unal.edu.co>
2021-12-13 15:36:41 -0500

    Fixed development listview

--- medsource/views/developmentListView.py
+++ medsource/views/developmentListView.py
@@ -8,4 +8,24 @@ from rest_framework import generics
 
 class DevelopmentListView(generics.ListAPIView):
     serializer_class = ShowDevelopmentSerializer
-    queryset = Development.objects.all()
+
+    def get_queryset(self):
+        patient = self.request.query_params.get('patient', None)
+        doctor = self.request.query_params.get('doctor', None)
+        nurse = self.request.query_params.get('nurse', None)
+        procedure = self.request.query_params.get('procedure', None)
+        hospital = self.request.query_params.get('hospital', None)
+        queryset = Development.objects.all()
+
+        if patient:
+            queryset = queryset.filter(patient__full_name__icontains=patient)
+        if doctor:
+            queryset = queryset.filter(doctor__identification=doctor)
+        if nurse:
+            queryset = queryset.filter(nurse__identification=nurse)
+        if procedure:
+            queryset = queryset.filter(procedure__name__icontains=procedure)
+        if hospital:
+            queryset = queryset.filter(hospital__name__icontains=hospital)
+
+        return queryset
